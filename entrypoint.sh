abc91331267977f69ad9c96ff13774a3fadc01ee
tomaer <i@tomaer.com>
2018-01-18 00:22:39 +0800

    调整脚本

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-export KETTLE_USER=$1 KETTLE_GROUP=$2
-
-if [ ! -n "$KETTLE_USER" ]||[ ! -n "$KETTLE_GROUP" ] ;then
-    KETTLE_USER="kettle"
-    KETTLE_GROUP="kettle"
-fi
 
 if [ ! -d "/opt/pdi-ce/account" ]; then
-    addgroup -S $KETTLE_GROUP && adduser -h /home/$KETTLE_USER -s /bin/ash -G $KETTLE_GROUP $KETTLE_USER && \
+    addgroup -S hadoop && adduser -h /home/hive -s /bin/ash -D -G hadoop hive && \
         echo "$KETTLE_USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers && \
         chown -R $KETTLE_USER:$KETTLE_GROUP $PDI_HOME/data-integration \
         chown -R $KETTLE_USER:$KETTLE_GROUP $PDI_HOME/datas && \
